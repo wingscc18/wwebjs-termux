db1d992fd3bb0cf857da0d5f704197963b8a1d4d
Joshua Dev <wingscc18@users.noreply.github.com>
2026-05-24 15:53:29 -0600

    Move QR zoom hint to QR event

--- events/qr.js
+++ events/qr.js
@@ -4,5 +4,6 @@ module.exports = {
         console.log('');
         console.log('Escanea este QR con WhatsApp:');
         context.qrcode.generate(qr, { small: context.config.qr.small });
+        console.log('\x1b[1;91m%s\x1b[0m', 'Si el QR no se ve bien, haz zoom hacia atras con dos dedos en la terminal para verlo completo.');
     }
 };
